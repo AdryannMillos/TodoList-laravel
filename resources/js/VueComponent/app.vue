10e91189d7c381cf0ad982ac65c8173a0c3b5ea8
AdryannMillos <adryann312@gmail.com>
2021-10-19 20:52:30 -0300

    first commit

---
+++ resources/js/VueComponent/app.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="toDoListContainer">
+    <div class="heading">
+        <h2 id="title">ToDo List</h2>
+        <add-item-form v-on:reloadlist="getList()" />
+    </div>
+      <list-view :items="items" v-on:reloadlist="getList()"/>
+</div>
+</template>
+
+<script>
+import addItemForm from "./addItemForm.vue"
+import listView from "./listView.vue"
+export default {
+    name: "app",
+   data(){
+       return{
+           items:[]
+       }
+
+   },
+    methods:{
+        getList(){
+            axios.get('api/items')
+            .then(response=>{
+                this.items = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    },
+    created(){
+        this.getList()
+    },
+   components: {
+       addItemForm,
+       listView
+   }
+}
+
+</script>
+
+<style scoped>
+.toDoListContainer{
+    width: 350px;
+    margin: auto;
+}
+.heading{
+    background: #e6e6e6;
+    padding: 10px;
+}
+#title{
+    text-align: center;
+}
+</style>
